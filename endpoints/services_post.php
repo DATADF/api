96ebd05841ebc18d6b90efe973a4c83b5f212b31
DATADF <klaytonfantin@outlook.com>
2019-07-08 17:10:55 -0300

    services

--- endpoints/services_post.php
+++ endpoints/services_post.php
@@ -1,24 +1,21 @@
 <?php
 
-    function api_services_post($request) {
+function api_services_post($request) {
 
-    $response = array(
-            'title' => 'Klayton',
-            'nome' => 'teste'
-        );
+    $response = $request['slug'];
 
-        return rest_ensure_response($response);
-    }
+    return rest_ensure_response($response);
+}
 
-    function register_api_service_post(){
-        register_rest_route('api/v2', '/services', array(
-            array(
-                'methods' => 'GET',
-                'callback' => 'api_services_post',
-            ),
-        ));
-    }
+function register_api_service_post(){
+    register_rest_route('api/v2', '/services', array(
+        array(
+            'methods' => 'GET',
+            'callback' => 'api_services_post',
+        ),
+    ));
+}
 
-    add_action('rest_api_init', 'register_api_service_post');
+add_action('rest_api_init', 'register_api_service_post');
 
 ?>
