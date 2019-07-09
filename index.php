0390cdce7cc2331dda37213dc9dc0bd876e408ea
DATADF <klaytonfantin@outlook.com>
2019-07-09 15:51:14 -0300

    style home

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php wp_head(); ?>
 
+<style>
+    .container {
+        width: 100%;
+        max-width: 960px;
+        height: 100%;
+        margin: auto;
+
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center
+    }
+</style>
+
 <div class="container">
 
-    <h2>API</h2>
+    <h2>API KOLI.TECH</h2>
+    <P>Acesse o painel administrativo:</P>
+    <a href="https://kolitech-site.dev/wp-admin">ADMIN</a>
 
 </div>
 
